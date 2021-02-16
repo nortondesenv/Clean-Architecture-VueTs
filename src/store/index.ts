bca99e2564a850110ce8c9eda48e9467618d9649
norton <57784722+nortondesenv@users.noreply.github.com>
2021-02-16 14:52:10 -0300

    feat: importa gerenciadores estados do produto

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+// modules
+import * as product from '@/store/modules/product';
+
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
-    modules: {},
+export interface RootState {
+    product: product.ProductState;
+}
+
+export default new Vuex.Store<RootState>({
+    modules: {
+        product: product.store,
+    },
 });
