1836b1b5c2549ee6f650a583b20fdb6f828fd7cc
norton <57784722+nortondesenv@users.noreply.github.com>
2021-02-16 16:30:41 -0300

    feat: adiciona component Banner

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="app">
         <Header />
+        <Banner />
         <transition name="fade-in-up">
             <router-view />
         </transition>
@@ -10,10 +11,12 @@
 
 <script lang="ts">
 import Header from '@/components/Modules/Header.vue';
+import Banner from '@/components/Modules/Banner.vue';
 
 export default {
     components: {
         Header,
+        Banner,
     },
 };
 </script>
---
+++ src/components/Modules/Banner.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="Banner">
+        <div class="Banner__Card">
+            <div class="Banner__Card__Content">
+                <img
+                    v-if="mobileMode"
+                    class="Banner__Card__Content__Image"
+                    src="@/../public/img/banner-mobile.png"
+                    alt="Logo"
+                />
+                <img
+                    v-else
+                    class="Banner__Card__Content__Image"
+                    src="@/../public/img/banner.png"
+                    alt="Logo"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    computed: {
+        mobileMode(): Boolean {
+            return this.$vssWidth < 470;
+        },
+    },
+});
+</script>
