c0e0573b2b9023a2ac5e2fac4bf06be5dd2a9e17
norton <57784722+nortondesenv@users.noreply.github.com>
2021-02-16 15:15:54 -0300

    refactor: limpa componentes default do cli

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <div class="Header"></div>
-    <transition name="fade-in-up">
-      <router-view />
-    </transition>
-    <div class="Cart"></div>
-  </div>
+    <div id="app">
+        <div class="Header"></div>
+        <transition name="fade-in-up">
+            <router-view />
+        </transition>
+        <div class="Cart"></div>
+    </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue';
 
 export default Vue.extend({});
 </script>
