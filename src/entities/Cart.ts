a665edbd3da0a1730037a38da6cb3fd95dd99bed
norton <57784722+nortondesenv@users.noreply.github.com>
2021-02-16 17:02:50 -0300

    feat: adiciona cart entiti

---
+++ src/entities/Cart.ts
@@ -0,0 +1,43 @@
+import { IProduct } from '@/entities/Product';
+import store from '@/store';
+import { AddItem, ClearItems, ToggleCart } from '@/store/modules/cart/types';
+
+export interface ICartItem extends IProduct {
+    checkoutPrice: number;
+    amount: number;
+}
+
+export interface ICart {
+    items: ICartItem[];
+}
+
+export class CartEntity {
+    constructor() {}
+
+    async toggleCart() {
+        await store.commit(new ToggleCart(store.state.cart.isCartOpen));
+    }
+
+    removeItem(index: number) {
+        store.state.cart.cart.items.splice(index, 1);
+    }
+
+    async addItemToCart(item: IProduct) {
+        const checkoutPrice = item.priceMember;
+        const amount = 1;
+        const cartItem = {
+            ...item,
+            checkoutPrice,
+            amount,
+        };
+        await store.commit(new AddItem(cartItem));
+    }
+
+    async checkOut() {
+        // Start operation
+
+        store.commit(new ClearItems());
+
+        return;
+    }
+}
