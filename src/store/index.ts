7b795e83218ec2ffa08f815884075f0f0296feff
norton <57784722+nortondesenv@users.noreply.github.com>
2021-02-16 16:44:01 -0300

    feat: import store cart

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,15 +3,18 @@ import Vuex from 'vuex';
 
 // modules
 import * as product from '@/store/modules/product';
+import * as cart from '@/store/modules/cart';
 
 Vue.use(Vuex);
 
 export interface RootState {
     product: product.ProductState;
+    cart: cart.CartState;
 }
 
 export default new Vuex.Store<RootState>({
     modules: {
         product: product.store,
+        cart: cart.store,
     },
 });
