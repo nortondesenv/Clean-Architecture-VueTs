a04b21f3740eab1b1e2f98e831acbdc24e5e2e50
norton <57784722+nortondesenv@users.noreply.github.com>
2021-02-16 18:50:57 -0300

    feat: adiciona home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,15 @@
 <template>
-    <h1>Home</h1>
+    <ProductsListContainer />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import ProductsListContainer from '@/containers/products/list/index.vue';
 
 export default Vue.extend({
     name: 'home',
+    components: {
+        ProductsListContainer,
+    },
 });
 </script>
