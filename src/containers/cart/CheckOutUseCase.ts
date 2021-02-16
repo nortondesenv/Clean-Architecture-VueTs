475d1669f905064ddc1dbb9d14c731660b298355
norton <57784722+nortondesenv@users.noreply.github.com>
2021-02-16 17:06:53 -0300

    feat: adiciona use case checkout

---
+++ src/containers/cart/CheckOutUseCase.ts
@@ -0,0 +1,28 @@
+import BaseUseCase from '@/usecases/BaseUseCase';
+import ErrorService from '@/services/ErrorService';
+import { CartEntity } from '@/entities/Cart';
+
+export interface ICheckOutUseCase {
+    cart: CartEntity;
+    errorService: ErrorService;
+}
+
+export default class CheckOutUseCase implements BaseUseCase {
+    cart: CartEntity;
+    errorService: ErrorService;
+
+    constructor({ cart, errorService }: ICheckOutUseCase) {
+        this.cart = cart;
+        this.errorService = errorService;
+    }
+
+    async execute() {
+        try {
+            await this.cart.checkOut();
+            alert('Obrigado e volte sempre!');
+        } catch (e) {
+            await this.errorService.handle(e);
+        }
+        return;
+    }
+}
