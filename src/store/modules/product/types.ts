2294974d0b0465575304478c55ba3bbc2e84425e
norton <57784722+nortondesenv@users.noreply.github.com>
2021-02-16 14:51:38 -0300

    feat: adiciona gerenciador de estados do produto

---
+++ src/store/modules/product/types.ts
@@ -0,0 +1,26 @@
+import { IProduct } from '@/entities/Product';
+
+export enum Types {
+    STORE_ITEM = 'product/store_item',
+    CLEAR_ITEM = 'product/clear_item',
+    STORE_ITEMS = 'product/store_items',
+    CLEAR_ITEMS = 'product/clear_items',
+}
+
+export class StoreItem implements FluxStandardAction {
+    type = Types.STORE_ITEM;
+    constructor(public payload: IProduct) {}
+}
+
+export class ClearItem implements FluxStandardAction {
+    type = Types.CLEAR_ITEM;
+}
+
+export class StoreItems implements FluxStandardAction {
+    type = Types.STORE_ITEMS;
+    constructor(public payload: List<IProduct>) {}
+}
+
+export class ClearItems implements FluxStandardAction {
+    type = Types.CLEAR_ITEMS;
+}
