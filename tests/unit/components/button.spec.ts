053800d0c750807d3b41d3825f1c168b9206e206
norton <57784722+nortondesenv@users.noreply.github.com>
2021-02-17 09:35:02 -0300

    test: adiciona testes Button

--- tests/unit/components/button.spec.ts
+++ tests/unit/components/button.spec.ts
@@ -2,20 +2,26 @@ import { shallowMount } from '@vue/test-utils';
 import Button from '@/components/Base/Button.vue';
 
 describe('Button.vue', () => {
+    const buttonText = 'Unit';
+    const wrapper = shallowMount(Button, {
+        propsData: { text: buttonText, disabled: false },
+    });
     it('Passagem de texto como prop', () => {
-        const buttonText = 'Adicionar';
-        const wrapper = shallowMount(Button, {
-            propsData: { text: buttonText },
-        });
         const div = wrapper.find('div');
         expect(div.text()).toMatch(buttonText);
     });
 
-    it('Emissão de evento click', async () => {
-        const wrapper = shallowMount(Button, {
-            propsData: { text: 'teste' },
-        });
-        await wrapper.trigger('click');
+    it('Emissão de evento click', () => {
+        wrapper.trigger('click');
         expect(wrapper.emitted().click).toBeTruthy();
+    });
+
+    it('Emissão de evento click bloqueado', async () => {
+        const wrapper = shallowMount(Button, {
+            propsData: { text: buttonText, disabled: true },
+        });
+
+        await wrapper.trigger('click');
+        expect(wrapper.emitted().click).toBeFalsy();
     });
 });
