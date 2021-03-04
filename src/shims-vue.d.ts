5eeb300a23145bc277fcc5b988d18970a48ee715
norton <57784722+nortondesenv@users.noreply.github.com>
2021-03-04 00:16:08 -0300

    chore(Apollo): adiciona apollo

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,12 @@
+import Vue from 'vue';
+import { DollarApollo } from 'vue-apollo/types/vue-apollo';
+
+declare module 'vue/types/vue' {
+    interface Vue {
+        $apollo: DollarApollo<this>;
+    }
+}
+
 declare module '*.vue' {
-    import Vue from 'vue';
     export default Vue;
 }
